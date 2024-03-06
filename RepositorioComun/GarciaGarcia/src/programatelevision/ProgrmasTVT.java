43be41586d9de07c9edf3613e4857d54360dbefa
Rosadelalb <rgargar414g.educaand.es>
2024-03-06 10:34:59 +0100

    Hola

---
+++ RepositorioComun/GarciaGarcia/src/programatelevision/ProgrmasTVT.java
@@ -0,0 +1,40 @@
+package programatelevision;
+
+import java.util.ArrayList;
+
+public class ProgrmasTVT {
+	
+	public String nombre;
+	public ArrayList <Temas> tema;
+	public String <Genero> genero;
+
+	
+
+	public ProgrmasTVT(String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ArrayList<Temas> getTemas(){
+		return null;
+	}
+	public ArrayList<Genero> getGenero(){
+		return null;
+	}
+	
+	public String getTema() {
+		return tema;
+	}
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+}
