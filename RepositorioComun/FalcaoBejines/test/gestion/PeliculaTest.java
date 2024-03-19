d9685644e86c606e19792bb32ee28e856f9ce65c
Irene <Irene@PC-CLASE>
2024-03-19 08:48:03 +0100

    Comentarios Javadoc

--- RepositorioComun/FalcaoBejines/test/gestion/PeliculaTest.java
+++ RepositorioComun/FalcaoBejines/test/gestion/PeliculaTest.java
@@ -1,15 +1,29 @@
-package test;
+package gestion;
 
 import junit.framework.TestCase;
 
+/**
+ * Clase de pruebas unitarias para la clase Pelicula.
+ * 
+ * @author Irene
+ * @version 17 18/03/2024
+ */
 public class PeliculaTest extends TestCase {
 	
+	/**
+     * Prueba el constructor de la clase Pelicula.
+     * Verifica que se inicialice correctamente el titulo de la pelicula y el director.
+     */
 	public void testPelicula() {
 		Pelicula p1 = new Pelicula("Star Wars Episodio I");
 		assertEquals("Star Wars Episodio I", p1.titulo);
 		assertEquals("Director desconocido", p1.director);
 	}
 	
+	/**
+     * Prueba el metodo getEspectador de la clase Pelicula.
+     * Verifica que se obtenga correctamente el nombre del espectador asociado a la pelicula.
+     */
 	public void testGetEspectador() {
 		Pelicula p2 = new Pelicula("Star Wars Episodio II");
 		Espectador e2 = new Espectador();
@@ -21,7 +35,6 @@ public class PeliculaTest extends TestCase {
 		//Obtener el nombre de la persona que ha visto la pelicula
 		//Espectador testEspectador = p2.getEspectador();
 		//String testNombre = testEspectador.getNombre();
-		
 		String testNombre = p2.getEspectador().getNombre();
 		assertEquals("Anakin", testNombre);
 	}
