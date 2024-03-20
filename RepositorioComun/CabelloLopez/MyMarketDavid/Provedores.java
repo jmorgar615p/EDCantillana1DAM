0e62e485037ca695e3f429e23e9a519050ead98c
dcablop767 <Usuario@TC2309011671>
2024-03-20 08:08:52 +0100

    Tercer Commit Completado

--- RepositorioComun/CabelloLopez/MyMarketDavid/Provedores.java
+++ RepositorioComun/CabelloLopez/MyMarketDavid/Provedores.java
@@ -1,6 +1,6 @@
 package MyMarketDavid;
 /**Declaracion de clases**/
-public class Provedores {
+public class Provedores implements InterfazProvedores {
 	private String nombre; 
 	private int maximoProductos; 
 /**Declaracion de propiedades de Provedores**/
@@ -9,24 +9,29 @@ public class Provedores {
 		maximoProductos = 10;
 	}
 /**Setter y Getter de Nombre**/
+	@Override
 	public void setNombre(String cualquierNombre) {
 		nombre = cualquierNombre;
 		
 	}
+	@Override
 	public String getNombre() {
 		return nombre;
 		
 	}
 	/**Setter y Getter de "MaximoProductos**/
+	@Override
 	public void setMaximoProductos(int maximoProductos) {
 		this.maximoProductos = maximoProductos;
 	}
 	
+	@Override
 	public int getMaximoProductos() {
 		return maximoProductos;
 		
 	}
 	
+	@Override
 	public String toString() {
 		return this.getNombre()+ " ("+this.getMaximoProductos()+" Estanteria)";		
 	}
