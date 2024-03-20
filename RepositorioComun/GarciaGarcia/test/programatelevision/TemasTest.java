47b0c8da75a4af51de10628d918b4de70e55736e
Rosadelalb <148953303+Rosadelalb@users.noreply.github.com>
2024-03-20 10:36:24 +0100

    Add files via upload

---
+++ RepositorioComun/GarciaGarcia/test/programatelevision/TemasTest.java
@@ -0,0 +1,35 @@
+package programatelevision;
+
+import junit.framework.TestCase;
+
+public class TemasTest extends TestCase {
+	/**
+	 * Pruebo a crear un objeto de la clase temas
+	 * */
+	public void testTemas() {
+		Temas g1 = new Temas("GG","Pepe");
+		assertEquals("GG", g1.nombre);
+		assertEquals("Pepe", g1.autor);
+	}
+	/**
+	 * pruebo seters
+	 * */
+	public void testSetNombre() {
+		Temas g2 = new Temas("Drama","Manue");
+		g2.setNombre("Cocina");
+		assertEquals("Cocina", g2.getNombre());
+	}
+	
+	public void testSetAuthor() {
+		InterfazAutores g3 = new Temas("Drama","Marcelo");
+		g3.setAuthor("Cocina");
+		assertEquals("Cocina", g3.getAuthor());
+	}
+	
+	public void testSetGenero() {
+		Temas g4 = new Temas("Accion","Marika");
+		Genero t1 = new Genero("Comedia",5);
+		g4.setGenero(t1);
+		assertEquals(t1, g4.genero);
+	}
+}
